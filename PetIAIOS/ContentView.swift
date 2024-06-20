7b5ec257ebf628f09dabefb0290f082edd01ecad
Felipe Lima <felipe.pedroso2108@gmail.com>
2024-06-20 20:20:43 -0300

    Initial Commit

---
+++ PetIAIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PetIAIOS
+//
+//  Created by Felipe Lima on 20/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PetIAIOS/PetIAIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetIAIOSApp.swift
+//  PetIAIOS
+//
+//  Created by Felipe Lima on 20/06/24.
+//
+
+import SwiftUI
+
+@main
+struct PetIAIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
