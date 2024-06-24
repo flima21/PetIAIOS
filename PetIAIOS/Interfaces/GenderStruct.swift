5dffe083a86562eac69913c21d6a64e7665c5060
Felipe Lima <felipe.pedroso2108@gmail.com>
2024-06-24 08:55:23 -0300

    :memo: create interface enum para gender

--- PetIAIOS/Interfaces/GenderStruct.swift
+++ PetIAIOS/Interfaces/GenderStruct.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+enum GenderStruct: String, CaseIterable, Identifiable {
+    case feminine, masculine, other
+    var id: Self { self }
+    
+    public static func getCodeById(cases: GenderStruct) -> String {
+        if cases == self.other { return "O" }
+        if cases == self.feminine { return "F" }
+        if cases == self.masculine { return "M" }
+        
+        return ""
+    }
+    
+    public static func getIdByCode(code: String) -> GenderStruct {
+        if code == "F" { return GenderStruct.feminine }
+        if code == "M" { return GenderStruct.masculine}
+        if code == "O" { return GenderStruct.other }
+        
+        return GenderStruct.other
+    }
+}
